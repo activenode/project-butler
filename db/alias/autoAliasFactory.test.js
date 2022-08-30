6a1b34654860abade0fd01e1c0274a4a0877881b
Kai Klostermann <kai@odd.af>
2022-08-30 06:55:09 +0200

    Alias clean up Finished alias and auto alias classes Removed WIP files as they were not needed anymore

--- db/alias/autoAliasFactory.test.js
+++
@@ -1,8 +0,0 @@
-const autoAliasFactory = require("./autoAliasFactory");
-
-describe("autoAliasFactory", () => {
-   test("should return the first word with no prefix if index is 0", () => {
-      const result = autoAliasFactory.generateAutoAliasesForIndex(0);
-      expect(result).toEqual(["0", "elk"]);
-   });
-});
